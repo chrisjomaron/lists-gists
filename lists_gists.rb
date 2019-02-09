c67f86019b4cd965f35d90a8a94828415a67c834
cmj <cmj@candjsoftware.co.uk>
2019-02-09 21:09:04 +0000

    move defaults up; clean up comments

--- lists_gists.rb
+++ lists_gists.rb
@@ -12,7 +12,7 @@ require 'pp'
 GITHUB_API_STEM = 'https://api.github.com'.freeze
 
 # Parse the command line options, using Ruby built-in
-options = {}
+options = {:username => 'defunkt', :verbose => false}
 OptionParser.new do |opts|
   opts.banner = "Usage: lists_gists.rb [options]"
 
@@ -23,12 +23,11 @@ OptionParser.new do |opts|
   end
 
   # optional flag to show debug output
-  opts.on("-v", "--verbose", "Run verbosely") do |v|
-    options[:verbose] = v
+  opts.on("-v", "--verbose", "Run verbosely") do 
+    options[:verbose] = true
   end
 
   # override default username, ARG is mandatory if switch is used
-  options[:username] = 'defunkt'
   opts.on("-u", "--username=NAME", "specify username to poll for public gists") do |u|
     options[:username] = u
   end
@@ -41,16 +40,18 @@ username = options[:username].freeze
 pp "Options: #{options}" if @verbose
 
 # convenience method to return payload from GET request
-def fetch(url)
+def fetch_payload(url)
+  puts "Connecting to #{url}..." if @verbose
   uri = URI(url)
   response = Net::HTTP.get_response(uri)
 
   case response
   when Net::HTTPSuccess then
     puts "Response class = #{response.class.name}" if @verbose
+    puts "Received JSON payload:\n", response.body if @verbose
     response.body
   else
-    "Error code: #{response.value}"
+    puts "Error code: #{response.value}"
     exit
   end
 end
@@ -63,27 +64,26 @@ begin
   old.each do |gist|
     old_gists << { 'id' => gist['id'], 'updated_at' => gist['updated_at'] }
   end
-  puts "Read in OLD gists:\n", old_gists if @verbose
+  puts "Read in previously seen gists from file '#{GISTS_FILE}':\n", old_gists if @verbose
 rescue Errno::ENOENT
   puts "File #{GISTS_FILE} not found. No gists seen before?"
 end
 
 # Now fetch current gists from GitHub API
 gists_url = "#{GITHUB_API_STEM}/users/#{username}/gists"
-json_payload = JSON.parse(fetch(gists_url))
-puts json_payload if @verbose
+json_payload = JSON.parse(fetch_payload(gists_url))
 current_gists = []
 json_payload.each do |gist|
   current_gists << { 'id' => gist['id'], 'updated_at' => gist['updated_at'] }
 end
-puts "Found CURRENT gists:\n", current_gists if @verbose
+puts "Parsed CURRENT gists from API:\n", current_gists if @verbose
 
 # diff the two, to find original or updated gists
-puts 'Calculating difference between lists:' if @verbose
+puts 'Calculating difference between old and new:' if @verbose
 new_gists = []
 current_gists.each do |gist|
   if old_gists.include? gist
-    puts "Discarded known gist #{gist['id']}" if @verbose
+    puts "Discarded already known gist #{gist['id']}" if @verbose
   else
     puts "Found NEW gist #{gist['id']}"
     new_gists << gist
