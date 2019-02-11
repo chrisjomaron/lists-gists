c13258448c6c325aa6b40e1d6ea935a06511dc77
chris.jomaron <chris.jomaron@moneysupermarket.com>
2019-02-11 11:44:59 +0000

    address rubocop violations; switch to URL instead of ID, for easier viewing of gists

--- lists_gists.rb
+++ lists_gists.rb
@@ -12,45 +12,42 @@ require 'pp'
 GITHUB_API_STEM = 'https://api.github.com'.freeze
 
 # Parse the command line options, using Ruby built-in
-options = {:username => 'defunkt', :verbose => false}
+options = { username: 'defunkt', verbose: false }
 OptionParser.new do |opts|
-  opts.banner = "Usage: lists_gists.rb [options]"
+  opts.banner = 'Usage: lists_gists.rb [options]'
 
   # display usage banner
-  opts.on("-h", "--help", "Show this help message") do
+  opts.on('-h', '--help', 'Show this help message') do
     puts opts
     exit
   end
 
   # optional flag to show debug output
-  opts.on("-v", "--verbose", "Run verbosely") do 
+  opts.on('-v', '--verbose', 'Run verbosely') do
     options[:verbose] = true
   end
 
   # override default username, ARG is mandatory if switch is used
-  opts.on("-u", "--username=NAME", "specify username to poll for public gists") do |u|
+  opts.on('-u', '--username=NAME', 'username to poll for public gists') do |u|
     options[:username] = u
   end
-
 end.parse!
 
 # set up variables
 @verbose = options[:verbose]
 username = options[:username].freeze
-previous_gists, current_gists, new_gists = [], [], []
+previous_gists = []
+current_gists = []
 
 pp "Options: #{options}" if @verbose
 
 # Convenience method to return payload from GET request
 def fetch_payload(url)
   puts "Connecting to #{url}..." if @verbose
-  uri = URI(url)
-  response = Net::HTTP.get_response(uri)
-
+  response = Net::HTTP.get_response(URI(url))
   case response
   when Net::HTTPSuccess then
-    puts "Response class = #{response.class.name}" if @verbose
-    puts "Received JSON payload:\n", response.body if @verbose
+    puts 'Received JSON payload:', response.body if @verbose
     response.body
   else
     puts "Error code: #{response.value}"
@@ -58,21 +55,21 @@ def fetch_payload(url)
   end
 end
 
-# Receives JSON and returns array of gist hashes (gist ids and timestamps)
+# Receives JSON and returns array of gist hashes (gist urls and timestamps)
 def json_to_array(json_payload)
   array = []
   json_payload.each do |line|
-    array << { 'id' => line['id'], 'updated_at' => line['updated_at'] }
+    array << { 'url' => line['url'], 'updated_at' => line['updated_at'] }
   end
   array
 end
 
-# Attempt to populate an array of previous gists, read from the user's state file
+# Attempt to populate array of previous gists, read from the user's state file
 GISTS_FILE = "#{username}.json".freeze
 begin
   old = JSON.parse(File.read(GISTS_FILE))
   previous_gists = json_to_array(old)
-  puts "Read in previously seen gists from file '#{GISTS_FILE}':", previous_gists if @verbose
+  puts "Read previous gists from '#{GISTS_FILE}':", previous_gists if @verbose
 rescue Errno::ENOENT
   puts "File '#{GISTS_FILE}' not found. No gists seen before?"
 end
@@ -81,14 +78,14 @@ end
 gists_url = "#{GITHUB_API_STEM}/users/#{username}/gists"
 json_payload = JSON.parse(fetch_payload(gists_url))
 current_gists = json_to_array(json_payload)
-puts "Parsed CURRENT gists from API:", current_gists if @verbose
+puts 'Parsed CURRENT gists from API:', current_gists if @verbose
 
 puts 'Calculating difference between previous and current:' if @verbose
 new_gists = current_gists - previous_gists
 deleted_gists = previous_gists - current_gists
 
 if new_gists.empty?
-  puts "Sorry, no new gists for #{username}" 
+  puts "Sorry, no new gists for #{username}"
 else
   puts 'Found new gists:', new_gists
 end
